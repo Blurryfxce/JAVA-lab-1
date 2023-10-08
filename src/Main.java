40da7e9b0ad2f2992de5a4785d14bf5d6a5db015
Olexander Koval <kowalsan4ik@gmail.com>
2023-10-08 21:14:34 +0300

    Created a Book class with the properties

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,18 @@
+class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private int year;
+
+    public Book(String title, String author, String isbn, int year) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.year = year;
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
