6905d6fc08deced3a74b3ed78c82dbaca57315ab
Olexander Koval <kowalsan4ik@gmail.com>
2023-10-08 21:54:13 +0300

    Added logic to Library class methods

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,14 @@ class Book {
         this.year = year;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
 }
 
 class Library {
@@ -21,20 +29,41 @@ class Library {
 
     public void addBook(Book book) {
         books.add(book);
-        // Додавання книги до бібліотеки
+        books.add(book);
+        System.out.println("Книга додана до бібліотеки.");
     }
 
     public void displayBooks() {
-        // Відображення всіх книг в бібліотеці
+        System.out.println("Книги в бібліотеці:");
+        for (Book book : books) {
+            System.out.println(book);
+        }
     }
 
     public Book findBookByTitle(String title) {
-        // Пошук книги за назвою
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                return book;
+            }
+        }
         return null;
     }
 
     public void removeBookByIsbn(String isbn) {
-        // Видалення книги за ISBN
+        Book bookToRemove = null;
+        for (Book book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                bookToRemove = book;
+                break;
+            }
+        }
+
+        if (bookToRemove != null) {
+            books.remove(bookToRemove);
+            System.out.println("Книга з ISBN " + isbn + " видалена з бібліотеки.");
+        } else {
+            System.out.println("Книгу з ISBN " + isbn + " не знайдено в бібліотеці.");
+        }
     }
 }
 public class Main {
